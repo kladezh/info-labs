a3e209731fa13b62fd5d376b14519ed76d58e6c4
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-08 16:25:37 +0300

    Add completed lab 4 of sem 4.

---
+++ SEM4-LR4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TestLabNET
+{
+    internal class Program
+    {
+        static readonly char[] SENTENSE_SEPARATORS = { '.', '!', '?', ':', ';' };
+
+        static void Main(string[] args)
+        {
+            string filepath = "C:\\Users\\yamsh\\Desktop\\test.txt";
+            Console.WriteLine("Путь к файлу:");
+            Console.WriteLine(filepath);
+
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader(filepath);
+
+                string content = reader.ReadToEnd();
+
+                Console.WriteLine("\nСодержимое файла:");
+                Console.WriteLine(content);
+
+                string[] sentences = content.Split(SENTENSE_SEPARATORS, StringSplitOptions.RemoveEmptyEntries);
+
+                Console.WriteLine("\nПредложения, не содержащие запятых:");
+                foreach (string sentence in sentences)
+                {
+                    if (!sentence.Contains(','))
+                        Console.WriteLine($"{sentence.Trim()}.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.Message);
+            }
+            finally
+            {
+                if (reader != null) reader.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
