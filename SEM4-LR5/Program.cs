57e83710591902153dd6a8a64c03d6dcb7203f9f
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-16 13:02:45 +0300

    Add sem 4 lab 5 and write basic file reading.

---
+++ SEM4-LR5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEM4_LR5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = "C:\\Users\\yamsh\\Desktop\\lab5.txt";
+
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader(filepath);
+
+                string content = reader.ReadToEnd();
+
+                Console.WriteLine(content);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("File exception: " + exc.Message);
+            }
+            finally
+            {
+                if (reader != null) reader.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
