9bc0907766358abf4be68f021f2bc7997c6b6553
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-16 13:33:04 +0300

    Put the reading file code at the method and isolate task code as well.

--- SEM4-LR5/Program.cs
+++ SEM4-LR5/Program.cs
@@ -10,48 +10,56 @@ namespace SEM4_LR5
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static string ReadFileContent(string filepath)
         {
-            string filepath = "C:\\Users\\yamsh\\Desktop\\lab5.txt";
-
             StreamReader reader = null;
             try
             {
                 reader = new StreamReader(filepath);
-
-                string content = reader.ReadToEnd();
-
-                string[] lines = content.Split('\n');
-
-                Console.WriteLine("Строки файла:");
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    Console.WriteLine($"[{i}] => " + lines[i]);
-                }
-
-                Regex regex = new Regex("\\*");
-
-                List<int> matchesLineIndexes = new List<int>();
-                for (int i = 0; i < lines.Length; i ++)
-                {
-                    if (regex.IsMatch(lines[i]))
-                    {
-                        matchesLineIndexes.Add(i);
-                    }
-                }
-
-                Console.WriteLine("\nНомера строк, в которых встречается символ \'*\'");
-                foreach (int i in matchesLineIndexes)
-                    Console.WriteLine(i);
+                return reader.ReadToEnd();
             }
             catch (Exception exc)
             {
                 Console.WriteLine("File exception: " + exc.Message);
+                return null;
             }
             finally
             {
                 if (reader != null) reader.Close();
             }
+        }
+
+        static void Task1()
+        {
+            string filepath = "C:\\Users\\yamsh\\Desktop\\lab5.txt";
+            string content = ReadFileContent(filepath);
+
+            string[] lines = content.Split('\n');
+
+            Console.WriteLine("Строки файла:");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine($"[{i}] => " + lines[i]);
+            }
+
+            Regex regex = new Regex("\\*");
+
+            List<int> matchesLineIndexes = new List<int>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (regex.IsMatch(lines[i]))
+                {
+                    matchesLineIndexes.Add(i);
+                }
+            }
+
+            Console.WriteLine("\nНомера строк, в которых встречается символ \'*\'");
+            foreach (int i in matchesLineIndexes)
+                Console.WriteLine(i);
+        }
+        static void Main(string[] args)
+        {
+            Task1();
 
             Console.ReadKey();
         }
