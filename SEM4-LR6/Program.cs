ff1418339615d6aac67408214e6f77a38b67639d
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-17 13:38:08 +0300

    Add Card's Suit and Rank enums for properties.

--- SEM4-LR6/Program.cs
+++ SEM4-LR6/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace SEM4_LR6
 {
+    internal enum Suit
+    {
+        Hearts,
+        Diamons,
+        Clubs,
+        Spades
+    }
+    internal enum Rank
+    {
+        Ace = 11,
+        King = 10,
+        Queen = 10,
+        Jack = 10,
+        Ten = 10,
+        Nine = 9,
+        Eight = 8,
+        Seven = 7,
+        Six = 6,
+        Five = 5,
+        Four = 4,
+        Three = 3,
+        Two = 2
+    }
+
     internal class Card
     {
-        private int _suit;
-        private int _rank;
+        private Suit _suit;
+        private Rank _rank;
+
+        public Suit Suit
+        {
+            get => _suit;
+            set => _suit = value;
+        }
+
+        public Rank Rank
+        {
+            get => _rank;
+            set => _rank = value;
+        }
+
+        public Card(Suit suit, Rank rank)
+        {
+            _suit = suit;
+            _rank = rank;
+        }
     }
 
     internal class Deck
@@ -31,7 +73,7 @@ namespace SEM4_LR6
             _cards = new List<Card>(count);
         }
 
-        public static Deck CreateStandard() 
+        public static Deck CreateStandard()
         {
             const int Count = 54;
 
