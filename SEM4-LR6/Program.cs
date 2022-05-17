e16adaba4cbcf43dd3f937457e4dccc63aea4511
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-05-17 13:29:25 +0300

    Add basic Card and Deck classes and specify their behavior.

--- SEM4-LR6/Program.cs
+++ SEM4-LR6/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace SEM4_LR6
 {
+    internal class Card
+    {
+        private int _suit;
+        private int _rank;
+    }
+
+    internal class Deck
+    {
+        private List<Card> _cards;
+
+        public int Count
+        {
+            get => _cards.Count;
+        }
+
+        public Deck()
+        {
+            _cards = new List<Card>();
+        }
+
+        public Deck(int count)
+        {
+            _cards = new List<Card>(count);
+        }
+
+        public static Deck CreateStandard() 
+        {
+            const int Count = 54;
+
+            Deck deck = new Deck(Count);
+
+            // creating cards in loop ...
+
+            return deck;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Deck deck = Deck.CreateStandard();
         }
     }
 }
